6f57c4589cbf80676f78dc4b8c9d0420fea16733
CatABit2 <aldirpandi007@gmail.com>
2025-06-12 12:23:09 +0800

    Membuat constructor dan setPenjumlahan

--- Matematika.java
+++ Matematika.java
@@ -2,49 +2,30 @@ package hitung;
 
 public class Matematika {
 
-    // Method untuk menghitung penjumlahan
-    public int tambah(int a, int b) {
-        return a + b;
+    // Variabel bertipe double
+    private double bil1;
+    private double bil2;
+
+    // Konstruktor untuk mengisi nilai variabel bil1 dan bil2
+    public Matematika(double bil1, double bil2) {
+        this.bil1 = bil1;
+        this.bil2 = bil2;
     }
 
-    // Method untuk menghitung pengurangan
-    public int kurang(int a, int b) {
-        return a - b;
-    }
-
-    // Method untuk menghitung perkalian
-    public int kali(int a, int b) {
-        return a * b;
-    }
-
-    // Method untuk menghitung pembagian
-    public double bagi(int a, int b) {
-        if (b != 0) {
-            return (double) a / b;
-        } else {
-            System.out.println("Pembagi tidak boleh nol");
-            return 0;
-        }
+    // Method setPenjumlahan yang mengembalikan hasil penjumlahan bil1 + bil2
+    public double setPenjumlahan() {
+        return bil1 + bil2;
     }
 
     // Kelas MatematikaBeraksi yang berisi method main
     public static class MatematikaBeraksi {
         public static void main(String[] args) {
-            // Membuat objek Matematika
-            Matematika matematika = new Matematika();
+            // Membuat objek Matematika dan menginisialisasi bil1 dan bil2
+            Matematika matematika = new Matematika(10.5, 5.5);
 
-            // Menghitung dan menampilkan hasil operasi
-            int tambahHasil = matematika.tambah(10, 5);
-            System.out.println("Hasil Penjumlahan: " + tambahHasil);
-
-            int kurangHasil = matematika.kurang(10, 5);
-            System.out.println("Hasil Pengurangan: " + kurangHasil);
-
-            int kaliHasil = matematika.kali(10, 5);
-            System.out.println("Hasil Perkalian: " + kaliHasil);
-
-            double bagiHasil = matematika.bagi(10, 5);
-            System.out.println("Hasil Pembagian: " + bagiHasil);
+            // Menghitung dan menampilkan hasil penjumlahan
+            double hasilPenjumlahan = matematika.setPenjumlahan();
+            System.out.println("Hasil Penjumlahan: " + hasilPenjumlahan);
         }
     }
 }
